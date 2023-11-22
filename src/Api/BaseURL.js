f2fbffbb2336315ee74d82e8be913d3b01e3d195
ankitgreenusys <ankit@digitalnightowl.in>
2023-11-22 11:53:12 +0530

    URL Update

--- src/Api/BaseURL.js
+++ src/Api/BaseURL.js
@@ -1,8 +1,8 @@
 // const BaseURL = "https://bankingcomibackend.onrender.com/"
 // const BaseURL = "http://localhost:5000/"
 // const BaseURL = "http://192.168.29.177:3000/"
-const BaseURL = "http://192.168.1.13:3000/"
-// const BaseURL = "http://banking.shivshankartransport.xyz/";
+// const BaseURL = "http://192.168.1.13:3000/"
+const BaseURL = "http://banking.shivshankartransport.xyz/";
 // const BaseURL = "https://banking-d4l2.onrender.com/";
 
 export default BaseURL;
