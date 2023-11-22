e089c2575d67bd65d508b54c1b10274a63376191
ankitgreenusys <ankit@digitalnightowl.in>
2023-11-22 12:07:35 +0530

    URL

--- src/Api/BaseURL.js
+++ src/Api/BaseURL.js
@@ -2,7 +2,7 @@
 // const BaseURL = "http://localhost:5000/"
 // const BaseURL = "http://192.168.29.177:3000/"
 // const BaseURL = "http://192.168.1.13:3000/"
-const BaseURL = "http://banking.shivshankartransport.xyz/";
+const BaseURL = "https://banking.shivshankartransport.xyz/";
 // const BaseURL = "https://banking-d4l2.onrender.com/";
 
 export default BaseURL;
